90172748d94bcef72a057262f1cfd74a6756f57f
KevinMorize <webdevbox91@gmail.com>
2021-03-03 08:54:09 +0100

    responsive mobile version + favico

--- app.js
+++ app.js
@@ -35,8 +35,16 @@ async function main(withIp = true){
         .then(response => response.json())
         .then(json => json)
         console.log('---weather---', weather);  
+
+
+    const time = await fetch(`http://api.timezonedb.com/v2.1/get-time-zone?key=V34ZZCX62O95&format=json&by=position&lat=${weather.coord.lat}&lng=${weather.coord.lon}`)
+        .then(result => result.json())
+        .then(json => json.formatted) 
+        // console.log('---time---', time)
+        
         
     displayWeatherInfos(weather)
+    timeByCity(time)
 }    
 
 function displayWeatherInfos(data){
@@ -45,13 +53,28 @@ function displayWeatherInfos(data){
     const main = data.weather[0].main;
     const description = data.weather[0].description;
     const wind = Math.round(data.wind.speed);
+    const humidity = data.main.humidity;
+    const pressure = data.main.pressure
 
     document.querySelector('#ville').textContent = name;
     document.querySelector('#température').textContent = temp + "°C";
     document.querySelector('#conditions').textContent = "(" + description + ")";
     document.querySelector('.wi').className = weatherIcons[main];
     document.querySelector('#wind').textContent =  "Wind : " + wind + " km/h";
+    document.querySelector('#humidity').textContent =  "Humidity : " + humidity + "%";
+    document.querySelector('#pressure').textContent =  "Pression : " + pressure + " hpa";
     document.body.className = main.toLowerCase();
+}   
+
+function timeByCity(value){
+    const hour = value.slice(11, 13).trim()
+    const min = value.slice(14, 16).trim()
+    const year = value.slice(0, 4).trim()
+    const month = value.slice(5, 7).trim()
+    const day = value.slice(8, 10).trim()
+
+
+    document.querySelector('#time').textContent = day + "/" + month + "/" + year + " - " + hour + " h " + min;
 }
 
 const ville = document.querySelector('#ville');
